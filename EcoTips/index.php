c2eb05ac1c7f083a2d74cac4fe581dbcb74acfdc
Steeven Michniewicz <48547593+SteevenMnw@users.noreply.github.com>
2020-11-04 09:44:07 +0100

    Add files via upload

---
+++ EcoTips/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+    <head>
+        <?php 
+    include('./header.html');
+        ?>
+    </head>
+    <body>
+        <a class="btn btn-primary btn-sm pull-right" href="connexion.php" role="button" aria-pressed="true" style="flex-direction: row-reverse;">
+            <span>
+                Se connecter
+            </span>
+        </a>
+        <div class="container container-login">
+            <div class="d-flex justify-content-center h-100">
+                <div class="centrer">
+                    <h1 style="color:white;"><br><br>
+                        Bienvenue sur le site Eco Tips<br>
+                        <font size="3pt">Partager vos tips avec toute la communauté !</font>
+                        <br>
+                        <a class="btn btn-primary btn-lg" href="tips.php" role="button" aria-pressed="true">
+                            <span>
+                                Proposer vos tips
+                                <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                    </h1> 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
